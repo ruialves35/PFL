f0b6f210f6aeaf77b73cad1e7925e7d9180abbd4
up201905853 <up201905853@fe.up.pt>
2021-11-13 15:44:41 +0000

    1.3 done i guess

--- Fib.hs
+++ Fib.hs
@@ -1,6 +1,13 @@
 module Fib where
 
- fibRec :: (Integral a) => a -> a
- fibRec 0 = 0
- fibRec 1 = 1
- fibRec n = fibRec(n-1) + fibRec(n-2)
+--1.1
+fibRec :: (Integral a) => a -> a
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec n = fibRec(n-1) + fibRec(n-2)
+
+--1.2
+
+--1.3
+fibListaInfinita :: [Integer]
+fibListaInfinita = 0 : 1 :  [a+b | (a,b)<-zip fibListaInfinita (tail fibListaInfinita)]
