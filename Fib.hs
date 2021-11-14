af749f78862ebebcf3cf26e1a169b0ff1f2d975e
up201905853 <up201905853@fe.up.pt>
2021-11-14 14:48:47 +0000

    Done 1.2 task Fib With DP

--- Fib.hs
+++ Fib.hs
@@ -7,8 +7,9 @@ fibRec 1 = 1
 fibRec n = fibRec(n-1) + fibRec(n-2)
 
 --1.2
---fibLista :: (Integral a) => a -> a
---fibLista n =
+fibLista :: (Integral a) => a -> a
+fibLista n = lista !! fromIntegral n
+  where lista = 0 : 1 : map(\n -> (lista !! (n-1)) + (lista !! (n-2)) ) [2..]
 
 --1.3
 fibListaInfinita :: (Integral a) => a -> a
