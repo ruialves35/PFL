0c8e492be40214c54d4f9bf5151375b9c75844a3
up201905853 <up201905853@fe.up.pt>
2021-11-14 15:58:55 +0000

    Fixed FibLista

---
+++ BigNumber.hs
@@ -0,0 +1,1 @@
+module BigNumber where
--- Fib.hs
+++ Fib.hs
@@ -1,17 +1,19 @@
 module Fib where
 
---1.1
+-- fibRec calculates the fibonacci value of the element n using recursion
 fibRec :: (Integral a) => a -> a
 fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec(n-1) + fibRec(n-2)
 
---1.2
+
+-- fibLista calculates the fibonacci value of the element n using dynamic programming
 fibLista :: (Integral a) => a -> a
 fibLista n = lista !! fromIntegral n
-  where lista = 0 : 1 : map(\n -> (lista !! (n-1)) + (lista !! (n-2)) ) [2..]
+  where lista = 0 : 1 : map(\n -> (lista !! (n-1)) + (lista !! (n-2)) ) [2.. fromIntegral n]
 
---1.3
+
+-- fibListaInfinita calculates the fibonacci value of the element n using an infinite list
 fibListaInfinita :: (Integral a) => a -> a
-fibListaInfinita n = fromIntegral (lista !! (fromIntegral n))
-  where lista = 0 : 1 :  [a+b | (a,b) <- zip lista (tail lista)]
+fibListaInfinita n = lista !! (fromIntegral n)
+  where lista = 0 : 1 :  [a + b | (a,b) <- zip lista (tail lista)]
