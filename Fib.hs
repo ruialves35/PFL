5b3ecee117b397f3715cc7b3bc8754f859dd8d29
monkin77 <monkinsmurf@gmail.com>
2021-11-26 13:14:04 +0000

    Added safeguards for negative numbers to return invalid

--- Fib.hs
+++ Fib.hs
@@ -2,30 +2,71 @@ module Fib where
 
 import BigNumber
 
+------------------------------------------------------------------------
+-------------------------- Int -----------------------------------------
+------------------------------------------------------------------------
+
 -- fibRec calculates the fibonacci value of the element n using recursion
+-- If given an invalid value, returns -1
 -------------------------------------------------------------------------------------
-fibRec :: (Integral a) => a -> a
+fibRec :: Int -> Int
 fibRec 0 = 0
 fibRec 1 = 1
-fibRec n = fibRec (n -1) + fibRec (n -2)
+fibRec n = if n < 0 then -1 else fibRec (n -1) + fibRec (n -2)
 
 -- fibLista calculates the fibonacci value of the element n using dynamic programming
+-- If given an invalid value, returns -1
 -------------------------------------------------------------------------------------
-fibLista :: (Integral a) => a -> a
-fibLista n = lista !! fromIntegral n
+fibLista :: Int -> Int
+fibLista n = if n < 0 then -1 else lista !! n
   where
-    lista = 0 : 1 : map (\n -> (lista !! (n -1)) + (lista !! (n -2))) [2 .. fromIntegral n]
+    lista = 0 : 1 : map (\n -> (lista !! (n -1)) + (lista !! (n -2))) [2 .. n]
 
 -- fibListaInfinita calculates the fibonacci value of the element n using an infinite list
+-- If given an invalid value, returns -1
 -------------------------------------------------------------------------------------
-fibListaInfinita :: (Integral a) => a -> a
-fibListaInfinita n = lista !! (fromIntegral n)
+fibListaInfinita :: Int -> Int
+fibListaInfinita n = if n < 0 then -1 else lista !! n
   where
     lista = 0 : 1 : [a + b | (a, b) <- zip lista (tail lista)]
 
+------------------------------------------------------------------------
+-------------------------- Integers ------------------------------------
+------------------------------------------------------------------------
+
+-- fibRec2 calculates the fibonacci value of the element n using recursion
+-- If given an invalid value, returns -1
+-------------------------------------------------------------------------------------
+fibRec2 :: Integer -> Integer
+fibRec2 0 = 0
+fibRec2 1 = 1
+fibRec2 n = if n < 0 then -1 else fibRec2 (n -1) + fibRec2 (n -2)
+
+-- fibLista2 calculates the fibonacci value of the element n using dynamic programming
+-- If given an invalid value, returns -1
+-------------------------------------------------------------------------------------
+fibLista2 :: Integer -> Integer
+fibLista2 n = if n < 0 then -1 else lista !! fromInteger n
+  where
+    lista = 0 : 1 : map (\n -> (lista !! (n -1)) + (lista !! (n -2))) [2 .. (fromInteger n)]
+
+-- fibListaInfinita2 calculates the fibonacci value of the element n using an infinite list
+-- If given an invalid value, returns -1
+-------------------------------------------------------------------------------------
+fibListaInfinita2 :: Integer -> Integer
+fibListaInfinita2 n = if n < 0 then -1 else lista !! fromInteger n
+  where
+    lista = 0 : 1 : [a + b | (a, b) <- zip lista (tail lista)]
+
+------------------------------------------------------------------------
+-------------------------- BIG NUMBERS --------------------------------
+------------------------------------------------------------------------
+
 -- fibRecBN calculates the fibonnaci of the n element by using recursion and BigNumbers
+-- If given an invalid value, returns -1
 -------------------------------------------------------------------------------------
 fibRecBN :: BigNumber -> BigNumber
+fibRecBN (False, _) = (False, [-1])
 fibRecBN (True, [0]) = (True, [0])
 fibRecBN (True, [1]) = (True, [1])
 fibRecBN bn = somaBN (fibRecBN bnRec1) (fibRecBN bnRec2)
@@ -36,7 +77,8 @@ fibRecBN bn = somaBN (fibRecBN bnRec1) (fibRecBN bnRec2)
 -- fibListaBN uses dynamic programming to calculate the bn-fibonacci element
 -- to do so, an auxiliar function was used in order to store the current values
 -- of the list with the elements and to create the breaking condition of the
--- list calculation
+-- list calculation.
+-- If given an invalid value, returns -1
 -------------------------------------------------------------------------------------
 fibListaBN :: BigNumber -> BigNumber
 fibListaBN bn = getIndex bn lista
@@ -61,6 +103,7 @@ helpFibListBN targetIdx lastIdx currList =
 
 -- fibListaInfinitaBN calculates the fibonnaci of the n element by using infinite lists
 -- applied to BigNumbers
+-- If given an invalid value, returns -1
 -------------------------------------------------------------------------------------
 fibListaInfinitaBN :: BigNumber -> BigNumber
 fibListaInfinitaBN bn = getIndex bn lista
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PFL
 PFL Project
+
+
+
+## Alínea 4
+com fibRec conseguimos correr ate n = 30, para cima desse 
+valor ja demora muito tempo.
+O mesmo acontece com FibRecBN.
+
+com fibLista conseguimos correr até n = 30000, para cima 
+desse valor ja demora muito tempo. Com fibListaBN so 
+da ate 5000 por isso esta mal implementado provavelmente,
+ou a eficiencia e baixa porque tive de fazer uma funcao
+so para a condicao de paragem e a funcao de comparacao
+de isBiggerModule é pouco eficiente, tal como a somaBN e subBN
+
+com fibListaInfinita chegamos facilmente ate n = 100000, e, 
+apesar de demorar mais, chegamos ate n = 2000000.
+com fibListaInfinitaBN chegamos até n = 10000
+Isto so mostra que as funcoes que usamos dos fibListaInfinita
+para buscar um indice e etc não são nada eficientes.
